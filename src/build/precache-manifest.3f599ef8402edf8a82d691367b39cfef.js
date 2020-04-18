6ea44562b99356070a87b7f7566ac15f1f2f25fa
Jagruti <mohitejagruti5@gmail.com>
2020-04-18 11:28:33 -0500

    first commit

---
+++ src/build/precache-manifest.3f599ef8402edf8a82d691367b39cfef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d23744b14fdffef57ea5ba4752a929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10fbf8dd98bd804fbbf",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "eb21b5816088ede3a3f6",
+    "url": "/static/js/2.8d356d5a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d356d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c10fbf8dd98bd804fbbf",
+    "url": "/static/js/main.a18eee89.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
